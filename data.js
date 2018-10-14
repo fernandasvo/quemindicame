1b00de543fc3da925cbaee8b3989be912d2195eb
LAPTOP-MOLO5AH5\ferna <fernandasvo@hotmail.com>
2018-10-14 00:36:01 -0300

    Front resultado de pesquisa lendo base de dados local

---
+++ data.js
@@ -0,0 +1,65 @@
+const bancoDados = {
+  "empresas": [
+    {//PERGUNTAR PRA QUE SERVE ESSA CHAVE
+    "empresa": {
+      "id": 1,
+      "usuarioProprietario": "fernandasvo",
+      "nome": "Serralheria Anthares",
+      "ramo": "Serralheria",
+      "servicos": ["Janelas", "Portões", "Corrimãos"],
+      "endereco": {
+        "rua": "Rua Rio de Janeiro, 129",
+        "cidade": "Santos",
+        "estado": "SP"
+      },//fim do objeto "endereco"//
+      "contato": {
+        "telefone": "+55 13 3232.1905",
+        "celular": "+55 13 98111.0186",
+        "email": "serralheriaanthares@uol.com.br",
+      },//fim do objeto "contato"//
+      "imgsServicos": [
+        "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwixoKam3oTeAhXJgJAKHegADGMQjRx6BAgBEAU&url=http%3A%2F%2Fnice.com.br%2Fportao-desbalanceado-veja-dicas-de-como-agir-neste-caso%2F&psig=AOvVaw3FZs2yC9yYRgGyzliZOC7m&ust=1539565426657092",
+        "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiq1cTE3oTeAhVIgpAKHUT9Ac8QjRx6BAgBEAU&url=http%3A%2F%2Fwww.serralheria.curitiba.emp.br%2Fjanelas-campo-comprido%2Fjanela-de-aluminio-para-quarto-campo-comprido&psig=AOvVaw2F_LLjjhtc50U3znXHD0PI&ust=1539565490313432",
+        "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj9mLDP3oTeAhUMlZAKHfvFBjkQjRx6BAgBEAU&url=https%3A%2F%2Fproduto.mercadolivre.com.br%2FMLB-740824009-corrimao-para-escada-aluminio-kit-1-metro-linear-branco-_JM&psig=AOvVaw1RFHV7JqekIm8ZniZ0IIaA&ust=1539565513869694",
+      ],//fim do array "imagensServicos"
+      "indicacoes": 2,
+    } //fim do objeto "empresa"
+  },//PERGUNTAR PRA QUE SERVE ESSA CHAVE
+
+  {//PERGUNTAR PRA QUE SERVE ESSA CHAVE
+  "empresa": {
+    "id": 2,
+    "usuarioProprietario": "fernandasvo",
+    "nome": "Bella Hair",
+    "ramo": "Cabeleireiro",
+    "servicos": ["Escova", "Manicure", "Pedicure"],
+    "endereco": {
+      "rua": "Rua Rio de Janeiro, 129",
+      "cidade": "Belo Horizonte",
+      "estado": "MG"
+    },//fim do objeto "endereco"//
+    "contato": {
+      "telefone": "+55 13 3232.1905",
+      "celular": "+55 13 98111.0186",
+      "email": "serralheriaanthares@uol.com.br",
+    },//fim do objeto "contato"//
+    "imgsServicos": [
+      "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwixoKam3oTeAhXJgJAKHegADGMQjRx6BAgBEAU&url=http%3A%2F%2Fnice.com.br%2Fportao-desbalanceado-veja-dicas-de-como-agir-neste-caso%2F&psig=AOvVaw3FZs2yC9yYRgGyzliZOC7m&ust=1539565426657092",
+      "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiq1cTE3oTeAhVIgpAKHUT9Ac8QjRx6BAgBEAU&url=http%3A%2F%2Fwww.serralheria.curitiba.emp.br%2Fjanelas-campo-comprido%2Fjanela-de-aluminio-para-quarto-campo-comprido&psig=AOvVaw2F_LLjjhtc50U3znXHD0PI&ust=1539565490313432",
+      "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj9mLDP3oTeAhUMlZAKHfvFBjkQjRx6BAgBEAU&url=https%3A%2F%2Fproduto.mercadolivre.com.br%2FMLB-740824009-corrimao-para-escada-aluminio-kit-1-metro-linear-branco-_JM&psig=AOvVaw1RFHV7JqekIm8ZniZ0IIaA&ust=1539565513869694",
+    ],//fim do array "imagensServicos"
+    "indicacoes": 2,
+  } //fim do objeto "empresa"
+}//PERGUNTAR PRA QUE SERVE ESSA CHAVE
+
+
+
+
+
+
+
+
+
+  ] //fim do array "empresas"
+  //"usuários" - array irmão do array "empresas"//
+}//fim do objeto bancoDados
