ff489f86925fa7835642c9857ff2fbeb72ef80b0
Fernanda Vaz <fernandasvo@hotmail.com>
2018-12-14 20:43:08 +0000

    Update index.js
    http only

--- backend/index.js
+++ backend/index.js
@@ -46,8 +46,8 @@ app.use("/api/usuarios", usuariosRoute);
 
 
 const PORT = process.env.PORT || 5000;
-http.createServer({},app).listen(PORT,() => console.log(`PORT :: ${PORT}`));
-https.createServer(app).listen(433,() => console.log('PORT :: 433'));
+http.createServer(app).listen(PORT,() => console.log(`PORT :: ${PORT}`));
+//https.createServer({},app).listen(433,() => console.log('PORT :: 433'));
 
 //Porta servidor
 
